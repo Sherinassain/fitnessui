345dd6c0febe97afd863da0efdf9e1fbc3141c32
sheirnass <shernassain@gmail.com>
2022-10-24 18:51:04 +0530

    commit

--- lib/pages/bottomtabs/plans_page.dart
+++ lib/pages/bottomtabs/plans_page.dart
@@ -202,7 +202,7 @@ class Plans_page extends StatelessWidget {
                           height: screensize.width * 0.05,
                         ),
                         //ads container
-
+//1
                         Ads_container(
                             imagepath:
                                 'https://www.mensjournal.com/wp-content/uploads/mf/the-truth-about-post-workout-nutrition-main.jpg?w=900&h=506&crop=1&quality=86&strip=all',
@@ -224,41 +224,41 @@ class Plans_page extends StatelessWidget {
                         //2
                         Ads_container(
                             imagepath:
-                                'https://www.mensjournal.com/wp-content/uploads/mf/the-truth-about-post-workout-nutrition-main.jpg?w=900&h=506&crop=1&quality=86&strip=all',
-                            maintitle1: 'Smart Diet &',
-                            maintitle2: 'Workout Plan',
+                                'https://thumbnails.yayimages.com/1600/c/ec4/cec4c28.jpg',
+                            maintitle1: 'Healthify',
+                            maintitle2: 'Transform',
                             maintitlecolor1: constantwhite,
-                            maintitlecolor2: constantwhite,
-                            subtitle: 'Starts at \$199 per month',
+                            maintitlecolor2: Colors.orange,
+                            subtitle: 'Starts at \$2500/m',
                             linemain:
-                                'Our Ai Powered plan to help you gain 3 kgs',
-                            icon1: Icons.call_to_action,
-                            line1: 'Customizable Diet Plan',
-                            icon2: Icons.run_circle,
-                            line2: 'Customizable Workout Routine',
-                            icon3: Icons.health_and_safety,
-                            line3: 'Ria-Your ai Coach & Assistant',
-                            buttoncolor: Colors.red,
-                            buttontitle: 'EXPLORE SMART'),
+                                'Pick your team of two coaches to help you',
+                            icon1: Icons.food_bank,
+                            line1: '2 Premiere Coaches',
+                            icon2: Icons.domain_verification_sharp,
+                            line2: 'HealthifyMe Smart Scale',
+                            icon3: Icons.call,
+                            line3: 'Unlimited Consultation Calls',
+                            buttoncolor: Colors.orange,
+                            buttontitle: 'KNOW MORE'),
                         //3
                         Ads_container(
                             imagepath:
-                                'https://www.mensjournal.com/wp-content/uploads/mf/the-truth-about-post-workout-nutrition-main.jpg?w=900&h=506&crop=1&quality=86&strip=all',
-                            maintitle1: 'Smart Diet &',
-                            maintitle2: 'Workout Plan',
+                                'https://thumbs.dreamstime.com/b/artistic-fitness-17412834.jpg',
+                            maintitle1: 'Healthify',
+                            maintitle2: 'Pro',
                             maintitlecolor1: constantwhite,
-                            maintitlecolor2: constantwhite,
-                            subtitle: 'Starts at \$199 per month',
+                            maintitlecolor2: Colors.blue,
+                            subtitle: 'Starts at \$3,333/m',
                             linemain:
-                                'Our Ai Powered plan to help you gain 3 kgs',
-                            icon1: Icons.call_to_action,
-                            line1: 'Customizable Diet Plan',
-                            icon2: Icons.run_circle,
-                            line2: 'Customizable Workout Routine',
+                                'Get fit faster, more consistently and holistically',
+                            icon1: Icons.security,
+                            line1: '2 Pro Coaches',
+                            icon2: Icons.punch_clock,
+                            line2: '1 CGM every ^ months',
                             icon3: Icons.health_and_safety,
-                            line3: 'Ria-Your ai Coach & Assistant',
-                            buttoncolor: Colors.red,
-                            buttontitle: 'EXPLORE SMART'),
+                            line3: 'HealthifyMe Smart Scale',
+                            buttoncolor: Colors.blue,
+                            buttontitle: 'KNOW MORE'),
                       ],
                     ),
                   )
